080a783f13844911e521bf7cefe589c52d52d878
karthi1229 <karthiking474@gmail.com>
2026-02-17 19:56:00 +0530

    Force CORS fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const cors = require("cors");
 
 app.use(cors({
   origin: [
@@ -13,7 +12,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
 
 app.use(express.json());
 
@@ -25,8 +23,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Internal Server Error" });
-});
-
